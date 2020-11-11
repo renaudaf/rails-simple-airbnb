cac76baf7fb253caf1943838de50bf012af461af
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-11 15:43:54 -0500

    index and show works

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,21 @@
 class FlatsController < ApplicationController
+ before_action :set_flat, only: [:show]
+
+ def index
+  @flats = Flat.all
+ end
+
+ def show
+ end
+
+ def new
+ 
+ end
+
+ private
+
+ def set_flat
+  @flat = Flat.find(params[:id])
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
- resources :flats
+  root to: 'flats#index'
+  resources :flats
 end
